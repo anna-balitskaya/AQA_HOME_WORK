1d55e04dc4d80b7775ad7140d2f22bceb2194be2
balit <balitskaya.anna1@gmail.com>
2025-02-10 22:31:39 +0100

    first commit

---
+++ src/lesson_5_2/Main.java
@@ -0,0 +1,8 @@
+package lesson_5_2;
+
+public class Main {
+    public static void main(String[] args) {
+        Park park = new Park("Thorpe Park");
+        park.parkInfo();
+    }
+}
---
+++ src/lesson_5_2/Park.java
@@ -0,0 +1,44 @@
+package lesson_5_2;
+
+public class Park {
+    private String name;
+
+    public class Attraction {  //вложенный нестатический класс
+        private String name_a;
+        private String information;
+        private String working_hours;
+        private double price;
+
+
+        Attraction(String name_a, String information, String working_hours, double price) {
+            this.name_a = name_a;
+            this.information = information;
+            this.working_hours = working_hours;
+            this.price = price;
+        }
+
+        public void attractionInfo() {
+            System.out.println("\nНазвание аттракциона: " + name_a + "\n" + "Информация о аттракционе: " + information + "\n" + "Время работы: " + working_hours + "\n" + "Стоимость: " + price);
+        }
+    }
+
+    public Park(String name) {
+        this.name = name;
+    }
+
+    public void parkInfo() {
+        Attraction attraction1 = new Attraction("Stealth", "The UK's Fastest Launch Coaster", "9AM-7PM", 35.50);
+        attraction1.attractionInfo();
+        Attraction attraction2 = new Attraction("Vortex", "Spin, swing and reach for the skies on Vortex", "9AM-7PM", 40.55);
+        attraction2.attractionInfo();
+        Attraction attraction3 = new Attraction("Storm Surge", "Take a spin on Storm Surge", "9AM-7PM", 34.00);
+        attraction3.attractionInfo();
+        Attraction attraction4 = new Attraction("Colossus", "The world’s first ten-loop roller coaster", "9AM-7PM", 45.00);
+        attraction4.attractionInfo();
+        Attraction attraction5 = new Attraction("Nemesis Inferno", "Thorpe Park's only inverted roller coaster", "9AM-7PM", 32.57);
+        attraction5.attractionInfo();
+
+        System.out.println();
+        System.out.println("Парк: " + name);
+    }
+}
