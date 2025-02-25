afed7649484084e6ef80aa459d3a57aebfa45d75
balit <balitskaya.anna1@gmail.com>
2025-02-25 14:51:32 +0100

    first commit

---
+++ src/Lesson_6/Plate.java
@@ -0,0 +1,26 @@
+package Lesson_6;
+
+public class Plate {
+    private int plateWithFood;
+
+    public Plate(int plate) {
+        this.plateWithFood = plate;
+    }
+
+    public void reduceAmount(int amount) {
+        if (amount <= plateWithFood) {
+            plateWithFood -= amount;
+        } else {
+            System.out.println("В тарелке не осталось еды");
+        }
+    }
+
+    public int getFoodAmount() {
+        return plateWithFood;
+    }
+
+    public void addFood (int food) {
+        System.out.println("В тарелку добавлено " + food + " еды");
+        plateWithFood = plateWithFood + food;
+    }
+}
